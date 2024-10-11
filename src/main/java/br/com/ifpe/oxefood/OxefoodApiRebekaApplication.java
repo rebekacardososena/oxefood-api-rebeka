cbed5cf167eef91e57b5d26310bce3cb848ebda1
aluno <aluno@ifpe>
2024-10-10 21:24:18 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRebekaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRebekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRebekaApplication.class, args);
+	}
+
+}
